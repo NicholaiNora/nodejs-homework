95a36a39c99b45ec40a1be7ce34b08ceabd89e03
NicholaiNora <nicholai.nora.dict@gmail.com>
2024-10-03 05:44:12 +0800

    NodeJS Homework 2

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,25 +1,85 @@
-import express from 'express';
+import express from "express";
+// prettier-ignore
+import { listContacts, getContactById, removeContact, addContact, updateContact } from "../../models/contacts.js";
+import { contactValidation } from "../../validation/validation.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// GET http://localhost:3000/api/contacts/
+router.get("/", async (_req, res, next) => {
+  try {
+    const result = await listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// GET http://localhost:3000/api/contacts/{id}
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await getContactById(contactId);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (!result) {
+      res.status(404).json({ message: "ID Not Found" });
+    }
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// POST http://localhost:3000/api/contacts/
+router.post("/", async (req, res, next) => {
+  try {
+    // Preventing lack of necessary data
+    const { error } = contactValidation.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: "missing required name field" });
+    }
+    const result = await addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE http://localhost:3000/api/contacts/{id}
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await removeContact(contactId);
+    if (!result) {
+      res.status(404).json({ message: "ID Not Found" });
+    }
+    res.json({
+      message: "Contact deleted",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    // Preventing lack of necessary data
+    const { error } = contactValidation.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: "missing required name field" });
+    }
+    const { contactId } = req.params;
+    const result = await updateContact(contactId, req.body);
+
+    if (!result) {
+      res.status(404).json({ message: "ID Not Found" });
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export { router };
